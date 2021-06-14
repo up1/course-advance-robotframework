13edfc69b846c2bbb20a088a6465f89832411aaa
up1 <somkiat.p@gmail.com>
2021-06-14 14:23:21 +0700

    Query data from db

--- 2021-workshop/db-mysql/demo-query.py
+++ 2021-workshop/db-mysql/demo-query.py
@@ -1,18 +1,30 @@
 import mysql.connector
 
-cnx = mysql.connector.connect(user='doadmin',
-                              password='mgfrwjggajh98th8',
-                              host='db-mysql-sgp1-69238-do-user-742153-0.b.db.ondigitalocean.com',
-                              port=25060,
-                              database='defaultdb')
-cursor = cnx.cursor()
 
-query = "SELECT id, first_name, last_name FROM employees"
-cursor.execute(query)
+def read_from_mysql():
+    cnx = mysql.connector.connect(user='doadmin',
+                                  password='mgfrwjggajh98th8',
+                                  host='db-mysql-sgp1-69238-do-user-742153-0.b.db.ondigitalocean.com',
+                                  port=25060,
+                                  database='defaultdb')
+    cursor = cnx.cursor()
 
-for (id, first_name, last_name) in cursor:
-    print("{}, {}, {}".format(
-        id, first_name, last_name))
+    query = "SELECT * FROM employees"
+    cursor.execute(query)
 
-cursor.close()
-cnx.close()
+    output = []
+    for (id, first_name, last_name, result, result_detail) in cursor:
+        print("{}, {}, {}, {}, {}".format(
+            id, first_name, last_name, result, result_detail))
+        c = {}
+        c['id'] = id
+        c['fname'] = first_name
+        c['lname'] = last_name
+        output.append(c)
+
+    cursor.close()
+    cnx.close()
+    return output
+
+
+read_from_mysql()
