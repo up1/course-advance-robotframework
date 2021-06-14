1af329b37b73e46d1fa38691e68c375ebdf1a41e
up1 <somkiat.p@gmail.com>
2021-06-14 14:41:28 +0700

    Update data in db

--- 2021-workshop/db-mysql/demo-query.py
+++ 2021-workshop/db-mysql/demo-query.py
@@ -1,6 +1,25 @@
 import mysql.connector
 
 
+def update_result_by_id(id, result, result_detail="Success"):
+    cnx = mysql.connector.connect(user='doadmin',
+                                  password='mgfrwjggajh98th8',
+                                  host='db-mysql-sgp1-69238-do-user-742153-0.b.db.ondigitalocean.com',
+                                  port=25060,
+                                  database='defaultdb')
+    cursor = cnx.cursor()
+
+    update_sql = ("UPDATE employees "
+                  "set result = %s, result_detail = %s "
+                  "where id = %s")
+    update_data = (result, result_detail, id)
+
+    cursor.execute(update_sql, update_data)
+    cnx.commit()
+    cursor.close()
+    cnx.close()
+
+
 def read_from_mysql():
     cnx = mysql.connector.connect(user='doadmin',
                                   password='mgfrwjggajh98th8',
@@ -27,4 +46,5 @@ def read_from_mysql():
     return output
 
 
-read_from_mysql()
+# update_result_by_id(1, 2, "Not OK")
+# read_from_mysql()
